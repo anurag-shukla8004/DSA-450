72657fa89c1fca2135a4c4e98c96585ab5f25824
anurag-shukla8004 <79192104+anurag-shukla8004@users.noreply.github.com>
2021-07-20 14:05:57 +0530

    Add files via upload

---
+++ DSA-450/array/array_5.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int arr[] = {1,2,3,4,-1,-2,-3,-4};
+    
+    sort(arr, arr+8);
+    
+    for(int i = 0; i < 8; i++){
+    cout<<arr[i];
+    }
+    
+    return 0;
+}
+ 
+
+   
+
+
